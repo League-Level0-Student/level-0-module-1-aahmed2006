222ff129ea4271250b5fbd758510aabe31be129f
aahmed2006 <league@WTS13.attlocal.net>
2018-09-29 17:28:07 -0700

    Badger, My Ages, Robot in Space

---
+++ src/ifs_ints_and_loops/Badger.java
@@ -0,0 +1,33 @@
+package ifs_ints_and_loops;
+
+public class Badger {
+	public static void main(String[] args) {
+		String b = "Badger";
+		String m = "Mushroom";
+		String s = "Snake";
+		for (int i = 0; i < 4; i++) {
+			for (int a = 0; a < 10; a++) {
+				System.out.println(b);
+			}
+			for (int a = 0; a < 2; a++) {
+				System.out.println(m);
+			}
+		}
+		for (int i = 0; i < 2; i++) {
+			for (int a = 0; a < 10; a++) {
+				System.out.println(b);
+			}
+			for (int a = 0; a < 5; a++) {
+				System.out.println(s);
+			}
+		}
+		for (int i = 0; i < 2; i++) {
+			for (int a = 0; a < 10; a++) {
+				System.out.println(b);
+			}
+			for (int a = 0; a < 2; a++) {
+				System.out.println(m);
+			}
+		}
+	}
+}
---
+++ src/ifs_ints_and_loops/MyAges.java
@@ -0,0 +1,9 @@
+package ifs_ints_and_loops;
+
+public class MyAges {
+	public static void main(String[] args) {
+		for(int i = 0; i < 13; i++) {
+			System.out.println(i);
+		}
+	}
+}
